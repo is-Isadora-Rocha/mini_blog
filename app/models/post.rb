b0f40cecd5a53f6a7137a0c6fd35dadad510d22d
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-26 12:43:35 -0300

    validates no model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :description, length: { maximum: 50 }
+  #usuário precisa escrever no máximo de 50 caracteres
+
+  before_create do
+    self.title =  "#{self.id} - #{self.title}"
+  end
 end
