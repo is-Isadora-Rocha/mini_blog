5865d10433cbbbb6bf175ba85fec757994a6b7cc
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-29 14:20:34 -0300

    model > validação de regras

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :description, presence: true
   validates :title, length: { minimum: 5 }
-  validates :description, length: { maximum: 50 }
-  #usuário precisa escrever no máximo de 50 caracteres
+  validates :description, length: { minimum: 15 }
+  #usuário precisa escrever no minimo 15 caracteres
 
   before_create do
     self.title =  "#{self.id} - #{self.title}"
